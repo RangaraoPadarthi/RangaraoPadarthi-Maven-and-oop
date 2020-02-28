a7396a9a75280bfbb5afa7d10404b4d3ec4d0a9e
RangaraoPadarthi <17pa1a05b9@vishnu.edu.in>
2020-02-28 21:30:11 +0530

    commit

---
+++ epamoops/src/main/java/com/ranga/epamoops/Jelly.java
@@ -0,0 +1,19 @@
+package com.ranga.epamoops;
+
+public class Jelly extends Sweet{
+
+	String Name;
+
+	int cost;
+
+	float weight;
+
+	int sweetness;
+
+	public Jelly(String Name,int cost,float weight,int sweetness) {
+
+		super(Name,cost,weight,sweetness);
+
+	}
+
+}
